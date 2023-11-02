d65ddeed38de61b1714edfd9c64b388302057f30
efrat miyara <efrat@tensor-tech.co.il>
2023-11-02 10:06:55 +0200

    back to defult llm model

--- main.py
+++ main.py
@@ -20,7 +20,7 @@ def main():
     if csv_file is not None:
 
         agent = create_csv_agent(
-            OpenAI(temperature=0, model_name="gpt-4"), csv_file, verbose=True)
+            OpenAI(temperature=0), csv_file, verbose=True)
 
         user_question = st.text_input("Ask a question about your CSV: ")
 
